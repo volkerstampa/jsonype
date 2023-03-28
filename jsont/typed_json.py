95be3315b2bdec2d05ad363369b8e7c2ca432381
Volker Stampa <Volker.Stampa@gmx.net>
2023-03-28 21:22:21 +0200

    Initial support for Unions

--- jsont/typed_json.py
+++ jsont/typed_json.py
@@ -1,4 +1,4 @@
-from typing import Any, Union, Sequence, Mapping, Type, TypeVar
+from typing import Any, Union, Sequence, Mapping, Type, TypeVar, get_origin, get_args, Callable, Iterable, Tuple
 
 JsonNull = type(None)
 JsonSimple = Union[int, float, str, bool]
@@ -6,6 +6,19 @@ JsonComplex = Union[Sequence["Json"], Mapping[str, "Json"]]
 Json = Union[JsonNull, JsonSimple, JsonComplex]
 
 T = TypeVar("T")
+R = TypeVar("R")
+
+NO_SUCCESS = object()
+
+
+def first_success(f: Callable[..., R], i: Iterable[Tuple]) -> Union[R, object]:
+    for args in i:
+        try:
+            return f(*args)
+        except ValueError:
+            pass
+    return NO_SUCCESS
+
 
 
 class TypedJson:
@@ -18,20 +31,31 @@ class TypedJson:
         raise ValueError(f"{o} of type {type(o)} no yet supported")
 
     def from_json(self, js: Json, cl: Type[T]) -> T:
+        if get_origin(cl) is Union:
+            return self._union_from_json(js, cl)
         if issubclass(cl, JsonNull):
             return self._null_from_json(js)
         if issubclass(cl, JsonSimple):
             return self._simple_from_json(js, cl)
         raise ValueError(f"{cl} as target type no yet supported")
 
+    def _union_from_json(self, js: Json, cl: Type[T]) -> T:
+        res = first_success(self.from_json, ((js, ty) for ty in get_args(cl)))
+        if res != NO_SUCCESS:
+            return res
+        raise ValueError(f"Cannot convert {js} to any of {get_args((cl))}")
+
+
 
     @staticmethod
-    def _simple_to_json(o: JsonSimple) -> Json:
-        return o
+    def _simple_to_json(js: JsonSimple) -> Json:
+        return js
 
     @staticmethod
-    def _simple_from_json(o: JsonSimple, cl: Type[JsonSimple]) -> Json:
-        return cl(o)
+    def _simple_from_json(js: JsonSimple, cl: Type[JsonSimple]) -> Json:
+        if isinstance(js, cl):
+            return cl(js)
+        raise ValueError(f"Cannot convert {js} to {cl}")
 
     @staticmethod
     def _null_to_json(o: None) -> None:
--- tests/jsont/test_typed_json.py
+++ tests/jsont/test_typed_json.py
@@ -1,13 +1,26 @@
+from typing import Union, Any, Type, Optional
 from unittest import TestCase, main
 
-from jsont.typed_json import TypedJson
+from jsont.typed_json import TypedJson, T
 
 
 class TypedJsonTestCase(TestCase):
+
+    typed_json = TypedJson()
+
     def test_simple(self):
-        typed_json = TypedJson()
         for simple_obj in [0, -1, 2, 0., 1., -2., True, False, "Hello", "", None]:
-            self.assertEqual(simple_obj, typed_json.from_json(typed_json.to_json(simple_obj), type(simple_obj)))
+            self.assert_can_convert_from_to_json(simple_obj, type(simple_obj))
+
+    def test_simple_with_union_type(self):
+        for i in [0, "Hello", None]:
+            self.assert_can_convert_from_to_json(i, Optional[Union[int, str]])
+
+    def assert_can_convert_from_to_json(self, simple_obj: Any, ty: Type[T]):
+        self.assertEqual(
+            simple_obj, self.typed_json.from_json(self.typed_json.to_json(simple_obj), ty))
+
+
 
 
 if __name__ == '__main__':

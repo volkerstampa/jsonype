f74727be7803ef7996c0b79903486713b4e9cff2
Volker Stampa <Volker.Stampa@gmx.net>
2023-03-28 22:06:54 +0200

    Support for array and objects
    - De-serialize arrays into/from sequences, tuples with
      or without precise type information
    - De-serialize objects into/from Mappings, TypedDicts with
      or without precise type information
    - Add test which randomly builds object trees for de-serialization

--- jsont/typed_json.py
+++ jsont/typed_json.py
@@ -1,24 +1,30 @@
-from typing import Any, Union, Sequence, Mapping, Type, TypeVar, get_origin, get_args, Callable, Iterable, Tuple
+from inspect import isclass, get_annotations
+from types import NoneType
+from typing import Any, Union, Sequence, Mapping, Type, TypeVar, get_origin, get_args, Callable, Iterable, Tuple, cast, \
+    List
 
-JsonNull = type(None)
+JsonNull = NoneType
 JsonSimple = Union[int, float, str, bool]
 JsonComplex = Union[Sequence["Json"], Mapping[str, "Json"]]
 Json = Union[JsonNull, JsonSimple, JsonComplex]
 
 T = TypeVar("T")
+S = TypeVar("S")
+LI = TypeVar("LI", bound=Sequence)
 R = TypeVar("R")
 
 NO_SUCCESS = object()
 
 
-def first_success(f: Callable[..., R], i: Iterable[Tuple]) -> Union[R, object]:
+def first_success(f: Callable[..., R], i: Iterable[Tuple]) -> Tuple[Union[R, object], Sequence[ValueError]]:
+    failures: List[ValueError] = []
     for args in i:
         try:
-            return f(*args)
-        except ValueError:
+            return f(*args), failures
+        except ValueError as e:
+            failures.append(e)
             pass
-    return NO_SUCCESS
-
+    return NO_SUCCESS, failures
 
 
 class TypedJson:
@@ -28,24 +34,32 @@ class TypedJson:
             return self._null_to_json(o)
         if isinstance(o, JsonSimple):
             return self._simple_to_json(o)
+        if isinstance(o, Sequence):
+            return self._sequence_to_json(o)
+        if isinstance(o, Mapping):
+            return self._mapping_to_json(o)
         raise ValueError(f"{o} of type {type(o)} no yet supported")
 
     def from_json(self, js: Json, cl: Type[T]) -> T:
-        if get_origin(cl) is Union:
+        origin_of_generic = get_origin(cl)
+        annotations = get_annotations(cl)
+        if cl is Any:
+            return js
+        if origin_of_generic is Union:
             return self._union_from_json(js, cl)
-        if issubclass(cl, JsonNull):
+        if cl is JsonNull:
             return self._null_from_json(js)
-        if issubclass(cl, JsonSimple):
+        if isclass(cl) and issubclass(cl, JsonSimple):
             return self._simple_from_json(js, cl)
-        raise ValueError(f"{cl} as target type no yet supported")
-
-    def _union_from_json(self, js: Json, cl: Type[T]) -> T:
-        res = first_success(self.from_json, ((js, ty) for ty in get_args(cl)))
-        if res != NO_SUCCESS:
-            return res
-        raise ValueError(f"Cannot convert {js} to any of {get_args((cl))}")
-
-
+        if isclass(origin_of_generic) and issubclass(origin_of_generic, tuple):
+            return self._tuple_from_json(js, cl)
+        if isclass(origin_of_generic) and issubclass(cast(type, origin_of_generic), Sequence):
+            return self._sequence_from_json(js, cl)
+        if isclass(origin_of_generic) and issubclass(cast(type, origin_of_generic), Mapping):
+            return self._mapping_from_json(js, cl)
+        if isclass(cl) and issubclass(cl, Mapping):
+            return self._typed_mapping_from_json(js, cl, annotations)
+        raise ValueError(f"{cl}{f' ({origin_of_generic})' if origin_of_generic else ''} as target type not supported")
 
     @staticmethod
     def _simple_to_json(js: JsonSimple) -> Json:
@@ -58,11 +72,88 @@ class TypedJson:
         raise ValueError(f"Cannot convert {js} to {cl}")
 
     @staticmethod
-    def _null_to_json(o: None) -> None:
-        return None
+    def _null_to_json(o: JsonNull) -> JsonNull:
+        return cast(JsonNull, None)
 
     @staticmethod
     def _null_from_json(js: Any) -> None:
         if isinstance(js, JsonNull):
             return None
-        raise ValueError(f"{js} cannot be converted to None")
+        raise ValueError(f"Cannot convert {js} to None")
+
+    def _union_from_json(self, js: Json, cl: Type[T]) -> T:
+        union_types = get_args(cl)
+        # a str is also a Sequence of str so check str first to avoid that
+        # it gets converted to a Sequence of str
+        union_types_with_str_first = ([str] if str in union_types else []) + [ty for ty in union_types if ty is not str]
+        res, failures = first_success(self.from_json, ((js, ty) for ty in union_types_with_str_first))
+        if res != NO_SUCCESS:
+            return res
+        raise ValueError(f"Cannot convert {js} to any of {union_types_with_str_first}: "
+                         f"{list(zip(union_types_with_str_first, failures))}")
+
+    def _sequence_to_json(self, li: Sequence[Any]) -> Sequence[Json]:
+        return [self.to_json(e) for e in li]
+
+    def _tuple_from_json(self, js: Json, cl: Type[T]) -> T:
+        element_types = get_args(cl)
+        if element_types.count(...) > 1:
+            raise ValueError(f"Cannot convert {js} to {cl} as {cl} has more than one ... parameter")
+        if isinstance(js, Sequence):
+            element_types = replace_ellipsis(element_types, len(js))
+            if len(js) != len(element_types):
+                raise ValueError(f"Cannot convert {js} to {cl} as number of type parameter do not match")
+            return tuple(self.from_json(e, ty) for e, ty in zip(js, element_types))
+        raise ValueError(f"Cannot convert {js} to {cl} as types are not convertible")
+
+    def _sequence_from_json(self, js: Json, cl: Type[T]) -> T:
+        element_types = get_args(cl) or (Any,)
+        assert len(element_types) == 1
+        if isinstance(js, Sequence):
+            return [self.from_json(e, element_types[0]) for e in js]
+        raise ValueError(f"Cannot convert {js} to {cl}")
+
+    def _mapping_to_json(self, o: Mapping[Any, Any]) -> Mapping[str, Json]:
+        def ensure_str(k: Any) -> str:
+            if isinstance(k, str):
+                return k
+            raise ValueError(f"Cannot convert {o} to json as it contains a non-str key: {k}")
+
+        return {ensure_str(k): self.to_json(v) for k, v in o.items()}
+
+    def _mapping_from_json(self, js: Json, cl: Type[T]) -> T:
+        key_value_types = get_args(cl) or (str, Any)
+        key_type, value_type = key_value_types
+        if key_type is not str:
+            raise ValueError(f"Cannot convert {js} to mapping with key-type: {key_type}")
+        if isinstance(js, Mapping):
+            return {k: self.from_json(v, value_type) for k, v in js.items()}
+        raise ValueError(f"Cannot convert {js} to {cl}")
+
+    def _typed_mapping_from_json(self, js: Json, cl: Type[T], annotations: Mapping[str, type]) -> T:
+        def type_for_key(k: str) -> type:
+            t = annotations.get(k)
+            if t:
+                return t
+            raise ValueError(f"Cannot convert {js} to {cl} as it contains unknown key {k}")
+
+        if isinstance(js, Mapping):
+            if cl.__required_keys__.issubset(frozenset(js.keys())):
+                return {k: self.from_json(v, type_for_key(k)) for k, v in js.items()}  # type: ignore
+            raise ValueError(f"Cannot convert {js} to {cl} "
+                             f"as it does not contain all required keys {cl.__required_keys__}")
+        raise ValueError(f"Cannot convert {js} to {cl}")
+
+
+def replace_ellipsis(element_types: Sequence[Type[T]], expected_len: int) -> Sequence[Type[T]]:
+    if ... in element_types:
+        element_types = fill_ellipsis(element_types, expected_len, Any)
+    return element_types
+
+
+def fill_ellipsis(types: Sequence[Type[T]], expected_len: int, fill_type: Type[T]) -> Sequence[Type[T]]:
+    types = list(types)
+    ellipsis_idx = types.index(...)
+    types[ellipsis_idx:ellipsis_idx+1]=[fill_type] * (expected_len - len(types) + 1)
+    return types
+
--- tests/jsont/test_typed_json.py
+++ tests/jsont/test_typed_json.py
@@ -1,12 +1,18 @@
-from typing import Union, Any, Type, Optional
+from random import choice, randint, uniform, randrange, choices, gauss
+from string import printable, digits, ascii_letters
+from sys import float_info
+from types import NoneType
+from typing import Union, Any, Type, Optional, Sequence, Mapping, TypedDict, Tuple, List, Callable
 from unittest import TestCase, main
 
 from jsont.typed_json import TypedJson, T
 
+ObjectFactory = Callable[[int, Sequence["ObjectFactory"]], Tuple[T, Type[T]]]
+
+typed_json = TypedJson()
+
 
 class TypedJsonTestCase(TestCase):
-
-    typed_json = TypedJson()
 
     def test_simple(self):
         for simple_obj in [0, -1, 2, 0., 1., -2., True, False, "Hello", "", None]:
@@ -16,11 +22,170 @@ class TypedJsonTestCase(TestCase):
         for i in [0, "Hello", None]:
             self.assert_can_convert_from_to_json(i, Optional[Union[int, str]])
 
-    def assert_can_convert_from_to_json(self, simple_obj: Any, ty: Type[T]):
-        self.assertEqual(
-            simple_obj, self.typed_json.from_json(self.typed_json.to_json(simple_obj), ty))
+    def test_homogeneous_sequence(self):
+        for li, ty in [
+            ([0, -1, 2], int),
+            ([0., 1., -2.], float),
+            ([True, False], bool),
+            (["Hello", ""], str),
+            ([None], NoneType)
+        ]:
+            self.assert_can_convert_from_to_json(li, Sequence[ty])
+
+    def test_inhomogeneous_sequence(self):
+        self.assert_can_convert_from_to_json([1, 0., True, None, "Hello"], Sequence[Union[int, float, bool, None, str]])
+
+    def test_homogeneous_mapping(self):
+        for m, ty in [
+            ({"k1": 1}, int),
+            ({"k1": True, "k2": False}, bool),
+            ({"k1": None}, NoneType)
+        ]:
+            self.assert_can_convert_from_to_json(m, Mapping[str, ty])
+
+    def test_inhomogeneous_mapping(self):
+        self.assert_can_convert_from_to_json({"k1": 1, "k2": "Demo"}, Mapping[str, Union[int, str]])
+
+    def test_typed_dict(self):
+        class Map(TypedDict):
+            k1: float
+            k2: int
+
+        self.assert_can_convert_from_to_json({"k1": 1., "k2": 2}, Map)
+
+    def test_random_objects(self):
+        for _ in range(500):
+            self.assert_can_convert_from_to_json(*(self._random_typed_object(8)))
+
+    def assert_can_convert_from_to_json(self, obj: Any, ty: Type[T]):
+        try:
+            self.assertEqual(
+                obj, typed_json.from_json(typed_json.to_json(obj), ty))
+        except AssertionError:
+            print(f"Cannot convert {obj} to {ty}")
+            raise
+
+    def _random_typed_object(self, size: int, factories: Optional[Sequence[ObjectFactory]] = None) -> Tuple[T, Type[T]]:
+        factories = factories or self._all_types_factories()
+        return choice(factories)(size, factories)
+
+    def _ambiguous_types_factories(self) -> Sequence[ObjectFactory]:
+        return (self._random_tuple,
+                self._random_tuple_with_ellipsis,
+                self._random_untyped_sequence,
+                self._random_untyped_map)
+
+    def _unambiguous_types_factories(self) -> Sequence[ObjectFactory]:
+        return (self._random_int,
+                self._random_float,
+                self._random_bool,
+                self._none,
+                self._random_str,
+                self._random_sequence,
+                self._random_map,
+                self._random_typed_map)
+
+    def _all_types_factories(self) -> Sequence[ObjectFactory]:
+        return tuple(self._ambiguous_types_factories()) + tuple(self._unambiguous_types_factories())
+
+    @staticmethod
+    def _random_int(_size: int, _factories: Sequence[ObjectFactory]) -> Tuple[int, Type[int]]:
+        return randint(-2 ** 63, 2 ** 63 - 1), int
+
+    @staticmethod
+    def _random_float(_size: int, _factories: Sequence[ObjectFactory]) -> Tuple[float, Type[float]]:
+        f = choices([-float_info.max,
+                     uniform(-float_info.max, 0),
+                     gauss(0, 10),
+                     uniform(0, float_info.max),
+                     float_info.max],
+                    weights=[1, 3, 5, 3, 1])
+        return f[0], float
+
+    @staticmethod
+    def _random_bool(_size: int, _factories: Sequence[ObjectFactory]) -> Tuple[bool, Type[bool]]:
+        return bool(randint(0, 1)), bool
+
+    @staticmethod
+    def _none(_size: int, _factories: Sequence[ObjectFactory]) -> Tuple[None, Type[NoneType]]:
+        return None, NoneType
+
+    @staticmethod
+    def _random_str(size: int, _factories: Sequence[ObjectFactory]) -> Tuple[str, Type[str]]:
+        return ''.join(choices(printable, k=randrange(size))), str
+
+    def _random_sequence(self, size: int, factories: Sequence[ObjectFactory]) -> Tuple[Sequence[T], Type[Sequence[T]]]:
+        seq, types = self._random_values(size, factories)
+        return list(seq), Sequence[Union[*types]]
+
+    def _random_untyped_sequence(self, size: int, factories: Sequence[ObjectFactory]) \
+            -> Tuple[Sequence[T], Type[Sequence]]:
+        unambiguous_factories = tuple(frozenset(self._unambiguous_types_factories()).intersection(frozenset(factories)))
+        seq, types = self._random_values(size, unambiguous_factories)
+        return list(seq), Sequence
+
+    def _random_tuple(self, size, factories: Sequence[ObjectFactory]) -> Tuple[Tuple[...], Type[Tuple[...]]]:
+        seq, types = self._random_values(size, factories)
+        return tuple(seq), Tuple[*types] if seq else Tuple[()]
+
+    def _random_tuple_with_ellipsis(self, size, factories: Sequence[ObjectFactory]) \
+            -> Tuple[Tuple[...], Type[Tuple[...]]]:
+        unambiguous_factories = tuple(frozenset(self._unambiguous_types_factories()).intersection(frozenset(factories)))
+        seq, types = self._random_values(size, unambiguous_factories)
+        return tuple(seq), Tuple[*insert_random_ellipsis(types)] if seq else Tuple[()]
+
+    def _random_map(self, size: int, factories: Sequence[ObjectFactory]) \
+            -> Tuple[Mapping[str, T], Type[Mapping[str, T]]]:
+        vals, types = self._random_values(size, factories)
+        return {self._random_str(size, factories)[0]: val for val in vals}, Mapping[str, Union[*types]]
+
+    def _random_untyped_map(self, size: int, factories: Sequence[ObjectFactory]) \
+            -> Tuple[Mapping[str, T], Type[Mapping]]:
+        unambiguous_factories = tuple(frozenset(self._unambiguous_types_factories()).intersection(frozenset(factories)))
+        vals, types = self._random_values(size, unambiguous_factories)
+        return {self._random_str(size, factories)[0]: val for val in vals}, Mapping
+
+    def _random_typed_map(self, size: int, factories: Sequence[ObjectFactory]) -> Tuple[TypedDict, Type[TypedDict]]:
+        vals, types = self._random_values(size, factories)
+        keys = [self._random_symbol() for _ in vals]
+        # noinspection PyPep8Naming
+        MapType = TypedDict(self._random_symbol(), dict(zip(keys, types)))
+        return MapType(**dict(zip(keys, vals))), MapType
+
+    @staticmethod
+    def _random_symbol() -> str:
+        return ''.join(choices(ascii_letters + digits, k=10))
+
+    def _random_values(self, size, factories: Sequence[ObjectFactory]) -> Tuple[Tuple[Any, ...], Tuple[Type[Any], ...]]:
+        previous_types: List[type] = []
+
+        def add_to_previous(val: Any, ty: type) -> Tuple[Any, type]:
+            previous_types.append(ty)
+            return val, ty
+
+        def cannot_convert(val: Any, ty: type) -> bool:
+            try:
+                typed_json.from_json(typed_json.to_json(val), ty)  # type: ignore
+                return False
+            except ValueError:
+                return True
+
+        def cannot_convert_to_previous_type(val: Any) -> bool:
+            return all(cannot_convert(val, ty) for ty in previous_types)
+
+        values_with_types = (self._random_typed_object(size // 2, factories) for _ in range(randrange(size)))
+        return list(zip(*(add_to_previous(val, ty)
+                          for val, ty in values_with_types
+                          if cannot_convert_to_previous_type(val)))) \
+            or ((), (Any,))
 
 
+def insert_random_ellipsis(types: Tuple[type, ...]) -> Sequence[type]:
+    ellipsis_start = randint(0, len(types) - 1)
+    ellipsis_end = randint(ellipsis_start + 1, len(types))
+    types = list(types)
+    types[ellipsis_start:ellipsis_end] = [...]
+    return types
 
 
 if __name__ == '__main__':

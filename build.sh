f9f6c7c65668cc83067106594e07809b147cc3bf
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2024-08-18 11:23:15 +0200

    Make build.sh Mac/BSD compatible
    Use stat -f instead of readlink to determine poetry's
    python interpreter

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env -S bash -eu -o pipefail
+#!/usr/bin/env bash
 
-"$(dirname "$(readlink --canonicalize-existing "$(which poetry)")")/python" -m setuptools_scm --force-write-version-files
+set -eu -o pipefail
+
+poetrysPython() {
+    local poetryAbsolutePath
+
+    if ! poetryAbsolutePath="$(readlink --canonicalize-existing "$(which poetry)" 2>/dev/null)"
+    then
+        # fallback for Mac/BSD without gnu-utils
+        poetryAbsolutePath="$(stat -f %R "$(which poetry)")"
+    fi
+    "$(dirname "$poetryAbsolutePath")/python" "$@"
+}
+
+# expects that poetry-setuptools-scm-plugin is added to poetry
+# with: poetry self add poetry-setuptools-scm-plugin
+poetrysPython -m setuptools_scm --force-write-version-files
 poetry install
 poetry build
 (cd docs && make clean html)
